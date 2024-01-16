ccae3c4ab47672dc1b063373e8cf920f012eb8a3
ZohaibFais <murtuza.tcc@gmail.com>
2024-01-16 16:38:39 +0500

    till q10

---
+++ index.js
@@ -0,0 +1,250 @@
+//Q1.
+
+// var userName = prompt("Please enter your name: ")
+// alert("Hi, "+ userName)
+
+// Q2.
+
+// var num = parseInt(prompt("Enter a number to print its multiplication table:  "))
+
+// var table1 = num * 1
+// var table2 = num * 2
+// var table3 = num * 3
+// var table4 = num * 4
+// var table5 = num * 5
+// var table6 = num * 6
+// var table7 = num* 7
+// var table8 = num * 8
+// var table9 = num * 9
+// var table10 = num * 10
+
+// if (num == 0 ){
+//     num = 5
+//     var table1 = num * 1
+//     var table2 = num * 2
+//     var table3 = num * 3
+//     var table4 = num * 4
+//     var table5 = num * 5
+//     var table6 = num * 6
+//     var table7 = num* 7
+//     var table8 = num * 8
+//     var table9 = num * 9
+//     var table10 = num * 10
+
+//     document.write("<h1> Multiplication table of 5</h1>")
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 1 + " " + "="  , table1)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 2 + " " + "="  , table2)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 3 + " " + "="  , table3)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 4 + " " + "="  , table4)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 5 + " " + "="  , table5)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 6 + " " + "="  , table6)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 7 + " " + "="  , table7)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 8 + " " + "="  , table8)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 9 + " " + "="  , table9)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 10 + " " + "="  , table10 )
+//     document.write("<br>")
+// }
+// else{
+//     var table1 = num * 1
+//     var table2 = num * 2
+//     var table3 = num * 3
+//     var table4 = num * 4
+//     var table5 = num * 5
+//     var table6 = num * 6
+//     var table7 = num* 7
+//     var table8 = num * 8
+//     var table9 = num * 9
+//     var table10 = num * 10
+
+//     document.write(" Multiplication table of: ",num)
+//     document.write("<br>")   
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 1 + " " + "="  , table1)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 2 + " " + "="  , table2)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 3 + " " + "="  , table3)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 4 + " " + "="  , table4)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 5 + " " + "="  , table5)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 6 + " " + "="  , table6)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 7 + " " + "="  , table7)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 8 + " " + "="  , table8)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 9 + " " + "="  , table9)
+//     document.write("<br>")
+//     document.write (num + " "+  "x" + " "+ 10 + " " + "="  , table10 )
+//     document.write("<br>")
+// }
+
+//Q3.
+
+// var city = prompt("Please enter your city: ")
+
+// if (city = "Karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+//Q4.
+
+// var gender = prompt("Please enter your gender: ")
+
+// if (gender == "male") {
+//     alert("Good Morning Sir.");
+// } else if (gender == "female") {
+//     alert("Good Morning Ma'am.");
+// }
+
+//Q5.
+
+// var color = prompt("Enter signal color: ")
+
+// if (color == "red"){
+//     alert("Vehicles must stop")
+// }else if (color == "yellow"){
+//     alert("Vehicles should get ready to move")
+// }
+// else if (color == "green") {
+//     alert("Vehicles can move now");
+// }
+
+//Q6.
+
+// var maxAge = parseInt(prompt("Enter max age: "))
+// var currentAge = parseInt(prompt("Enter your age: "))
+
+// if (currentAge<= maxAge) {
+//     alert("You are welcome ")
+// } else{
+//     alert("You are overage")
+// }
+
+//Q7.
+
+// var fuel = parseFloat(prompt("Enter remaining fuel: "))
+
+// if (fuel<0.25){
+//     alert("Please refill the fuel in your car")
+// }
+// else if (fuel > 0.25){
+//     alert("You are good to go")
+// }
+
+//Q9.
+
+// var a = 4; 
+// if (++a == 5){ 
+//     alert("given condition for variable a is true"); 
+// } 
+// var b = 82; 
+// if (b++ === 83){ 
+//     alert("given condition for variable b is true"); 
+// }
+
+// var c = 12; 
+// if (c++ === 13){ 
+//     alert("condition 1 is true"); 
+// } 
+// if (c === 13){ 
+//     alert("condition 2 is true"); 
+// } 
+// if (++c < 14){ 
+//     alert("condition 3 is true"); 
+// } 
+// if(c === 14){ 
+//     alert("condition 4 is true"); 
+// } 
+ 
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ 
+//     alert("The cost equals"); 
+// }
+// if (true){ 
+//     alert("True"); 
+// } 
+// if (false){ 
+//     alert("False"); 
+// }
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); 
+// }
+
+//Q9.
+
+// var chemMarks = parseFloat(prompt("Enter your marks in Chemistry: "))
+// var physMarks = parseFloat(prompt("Enter your marks in Physics: "))
+// var csMarks = parseFloat(prompt("Enter your marks in Computer: "))
+// var total = 300
+// var marksObtained = chemMarks + physMarks + csMarks
+
+// var percentage = (marksObtained/total)*100
+
+// document.write("<h1> Marks Sheet</h1>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total Marks: ",total)
+// document.write("<br>")
+// document.write("Marks Obtained: ",marksObtained)
+// document.write("<br>")
+
+// if (percentage >= 80){
+//     document.write("Grade: A-one")
+//     document.write("<br>")
+//     document.write("Remarks: Excellent")
+// } else if (percentage >= 70){
+//     document.write("Grade: A")
+//     document.write("<br>")
+//     document.write("Remarks: Good")
+// } else if (percentage >= 60){
+//     document.write("Grade: B")
+//     document.write("<br>")
+//     document.write("Remarks: You need to improve")
+// } else if (percentage < 60){
+//     document.write("Grade: Fail")
+//     document.write("<br>")
+//     document.write("Remarks: Sorry")
+// } 
+
+//Q10.
+var nameItem1 = prompt("Enter name of item: ")
+var nameItem2 = prompt("Enter name of item: ")
+var prItem1 = parseInt(prompt("Enter price of item1: "))
+var quanItem1 = parseInt(prompt("Enter quantity of item1"))
+var prItem2 = parseInt(prompt("Enter price of item2: "))
+var quanItem2 = parseInt(prompt("Enter quantity of item2"))
+var shippingCharges = 250
+
+var totalCost = (prItem1 *quanItem1) + (prItem2*quanItem2)+ shippingCharges
+if (totalCost > 2000){
+    var discountPrice = (totalCost * 90)/100
+}
+
+document.write("Price of" + " " + nameItem1 + " " + "is" + ":" ,prItem1)
+document.write("<br>")
+document.write("Quantity of" + " " + nameItem1 + " " + "is" + ":" ,quanItem1)
+document.write("<br>")
+document.write("Price of" + " " + nameItem2 + " " + "is" + ":" ,prItem2)
+document.write("<br>")
+document.write("Quantity of" + " " + nameItem2 + " " + "is" + ":" ,quanItem2)
+document.write("<br>")
+document.write("<br>")
+document.write("Shipping Charges: ",shippingCharges)
+document.write("<br>")
+document.write("<br>")
+document.write ("Total Cost of your order is" + " " + totalCost + " "+ "PKR")
